ecadc7348e1e257dd9807c3e78cb7abf785277c8
Neo Yin <yinzhang@berkeley.edu>
2024-11-07 12:03:03 -0800

    h5 class validity check

--- deepheme.py
+++ deepheme.py
@@ -513,7 +513,7 @@ if __name__ == "__main__":
 
     model = model_create(HemeLabel_ckpt_path)
 
-    test_image = "/media/greg/534773e3-83ea-468f-a40d-46c913378014/neo/results_dir/BMA-diff_2024-10-05 15:21:08/cells/M4/M4-E4-E1-MO2_8350-16.jpg"
+    test_image = "/media/greg/534773e3-83ea-468f-a40d-46c913378014/neo/results_dir/BMA-diff_2024-10-05 15:21:08/cells/ER1/ER1-ER2-L2-M1_10770-1.jpg"
 
     # Load the test image
     image = Image.open(test_image)
