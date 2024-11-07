755b1b29e446277666d84ceeece3c66b455a4f0d
Neo Yin <yinzhang@berkeley.edu>
2024-11-07 02:11:32 -0800

    h5 class validity check

--- deepheme.py
+++ deepheme.py
@@ -523,7 +523,7 @@ if __name__ == "__main__":
     import numpy as np
 
     # Open the image with PIL
-    image = Image.open('your_image.jpg').convert("RGB")
+    image = Image.open(test_image).convert("RGB")
 
     # Convert the image to a numpy array
     image_np = np.array(image)
