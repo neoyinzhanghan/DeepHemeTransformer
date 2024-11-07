1d4600f3741e47092f542bdc7ca5aecc5b08defc
Neo Yin <yinzhang@berkeley.edu>
2024-11-06 22:26:37 -0800

    h5 class validity check

--- check_cell_classes.py
+++ check_cell_classes.py
@@ -1,5 +1,6 @@
 import os
 import h5py
+import pandas as pd
 from tqdm import tqdm
 
 h5_dir_path = "/media/hdd3/neo/DeepHemeTransformerData"
@@ -8,6 +9,11 @@ h5_dir_path = "/media/hdd3/neo/DeepHemeTransformerData"
 h5_paths = [
     os.path.join(h5_dir_path, f) for f in os.listdir(h5_dir_path) if f.endswith(".h5")
 ]
+
+validity_check_results = {
+    "h5_name": [],
+    "unique_classes": [],
+}
 
 
 def check_cell_classes(h5_path):
@@ -30,19 +36,28 @@ def check_cell_classes(h5_path):
             or set(class_indices) != {0}
             or set(class_indices) != {21}
             or set(class_indices) != {}
-        )
+        ), class_indices
 
 
 valid_h5_files = []
 invalid_h5_files = []
 
 for h5_path in tqdm(h5_paths, desc="Checking cell classes..."):
-    if check_cell_classes(h5_path):
+    validity, class_indices = check_cell_classes(h5_path)
+    if validity:
         print(f"Found incorrect class indices in: {h5_path}")
         valid_h5_files.append(os.path.basename(h5_path))
     else:
         print(f"Correct class indices in: {h5_path}")
         invalid_h5_files.append(os.path.basename(h5_path))
 
+    validity_check_results["h5_name"].append(os.path.basename(h5_path))
+    validity_check_results["unique_classes"].append(set(class_indices))
+
 print(f"Found {len(valid_h5_files)} valid h5 files.")
 print(f"Found {len(invalid_h5_files)} invalid h5 files.")
+
+validity_check_results_df = pd.DataFrame(validity_check_results)
+validity_check_results_df.to_csv(
+    os.path.join(h5_dir_path, "validity_check_results.csv"), index=False
+)
