ee03db7a208c43baddd1d4684de0c93c0bbb2d57
Neo Yin <yinzhang@berkeley.edu>
2024-11-25 15:57:40 -0800

    gathering result folder with continue on error

--- DiffTransformerV1/get_differential_label.py
+++ DiffTransformerV1/get_differential_label.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from get_copath_data import get_diff, get_path_data
 
-metadata_path = "/media/hdd3/neo/DiffTransformerV1DataMini/wsi_metadata.csv"
+# metadata_path = "/media/hdd3/neo/DiffTransformerV1DataMini/wsi_metadata.csv"
+metadata_path = "/media/hdd3/neo/wsi_metadata.csv"
 metadata = pd.read_csv(metadata_path)
 
 # get the accession_numbers as a list of strings
@@ -21,4 +22,4 @@ diff = get_diff(path_df)
 print(f"Number of rows in differential data: {len(diff)}")
 
 # save the differential data to a csv file
-diff.to_csv("/media/hdd3/neo/DiffTransformerV1DataMini/diff_data.csv", index=False)
+diff.to_csv("/media/hdd3/neo/diff_data.csv", index=False)
