2120ef2dcc2dd8877a2a6b74ea75ebd58e9ef7ae
Neo Yin <yinzhang@berkeley.edu>
2024-11-25 15:48:38 -0800

    gathering result folder with continue on error

--- DiffTransformerV1/get_differential_label.py
+++ DiffTransformerV1/get_differential_label.py
@@ -1,1 +1,12 @@
-metadata_path = "/media/hdd3/neo/DeepHemeTransformerData/labelled_features_metadata.csv"
+import pandas as pd
+from get_copath_data import get_diff
+
+metadata_path = "/media/hdd3/neo/DiffTransformerV1DataMini/wsi_metadata.csv"
+metadata = pd.read_csv(metadata_path)
+
+# get the accession_numbers as a list of strings
+accession_numbers = metadata["accession_number"].tolist()
+
+diff = get_diff(accession_numbers)
+
+print(type(diff))
