9503e0671c85270770b89165006330f62f4bf392
Neo Yin <yinzhang@berkeley.edu>
2024-11-25 14:22:05 -0800

    DTv1 gather result_folders

---
+++ DiffTransformerV1/gather_result_folders.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+import pandas as pd
+from tqdm import tqdm
+
+metadata_file_path = (
+    "/media/hdd3/neo/DeepHemeTransformerData/labelled_features_metadata.csv"
+)
+results_dir = "/media/greg/534773e3-83ea-468f-a40d-46c913378014/neo/results_dir"
+
+save_dir = "/media/hdd3/neo/DiffTransformerV1Data"
+os.makedirs(save_dir, exist_ok=True)
+
+# open the metadata file
+metadata = pd.read_csv(metadata_file_path)
+
+# get the column result_dir_name as a list of strings
+result_dir_names = metadata["result_dir_name"].tolist()
+
+print(f"Found {len(result_dir_names)} result directories")
+
+for result_dir_name in tqdm(result_dir_names, desc="Copying result directories"):
+    result_dir = os.path.join(results_dir, result_dir_name)
+    save_path = os.path.join(save_dir, result_dir_name)
+    shutil.copytree(result_dir, save_path)
