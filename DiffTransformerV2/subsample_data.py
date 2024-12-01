8cfddd5b7a90cda661936730a9a21f1b3f983094
Neo Yin <yinzhang@berkeley.edu>
2024-11-30 23:26:00 -0800

    training script

--- DiffTransformerV2/subsample_data.py
+++ DiffTransformerV2/subsample_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-num_train = 1
+num_train = 10
 diff_data_path = "/media/hdd3/neo/DiffTransformerV1DataMini/diff_data.csv"
 diff_data = pd.read_csv(diff_data_path)
 
@@ -10,8 +10,8 @@ diff_data = diff_data.sample(frac=1).reset_index(drop=True)
 # add a split column
 diff_data["split"] = "train"
 
-# only keep the first num_train rows in the training set
-diff_data.loc[num_train:, "split"] = "val"
+# only keep the first num_train rows in the dataframe, drop the rest
+diff_data = diff_data[:num_train]
 
 # save the split data
 split_diff_data_path = (
