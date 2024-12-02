f9cc0fb2c14ab7ee482f9dba3571fce20394134e
Neo Yin <yinzhang@berkeley.edu>
2024-12-01 21:28:01 -0800

    lol

--- DiffTransformerV1/get_model_output.py
+++ DiffTransformerV1/get_model_output.py
@@ -5,9 +5,7 @@ from dataset import TensorStackDataModule
 
 model_ckpt_path = "/home/greg/Documents/neo/DeepHemeTransformer/DiffTransformerV1/lightning_logs/multihead_attention/version_0/checkpoints/epoch=499-step=500.ckpt"
 feature_stacks_dir = "/media/hdd3/neo/DiffTransformerV1DataMini/feature_stacks"
-diff_data_path = (
-    "/media/hdd3/neo/DiffTransformerV1DataMini/subsampled_split_diff_data.csv"
-)
+diff_data_path = "/media/hdd3/neo/DiffTransformerV1DataMini/split_diff_data.csv"
 
 data_module = TensorStackDataModule(
     feature_stacks_dir=feature_stacks_dir,
@@ -20,7 +18,7 @@ model = MultiHeadAttentionClassifierPL.load_from_checkpoint(
     model_ckpt_path, map_location="cuda"
 )
 data_module.setup()
-train_loader = data_module.train_dataloader()
+train_loader = data_module.val_dataloader()
 
 model.eval()
 
