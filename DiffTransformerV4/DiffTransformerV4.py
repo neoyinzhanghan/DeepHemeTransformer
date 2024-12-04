8c6bc2fbfc44aa8a2cb4c6ef9b48eacfc874e60e
Neo Yin <yinzhang@berkeley.edu>
2024-12-04 13:11:58 -0800

    new model debyug

--- DiffTransformerV4/DiffTransformerV4.py
+++ DiffTransformerV4/DiffTransformerV4.py
@@ -115,19 +115,19 @@ class MultiHeadAttentionClassifier(nn.Module):
         # add the logits and the offsets then we need to uniformly subtract 1/num_classes from the logits to make sure the sum is 1
         logits = logit_stack + logits_offsets - 1 / self.num_classes
 
-        print(f"logits shape: {logits.shape}")
-        print(logits[0, 0, :])
-        print(f"Sum of logits: {logits[0, 0, :].sum()}")
-        print(f"logit_stack shape: {logit_stack.shape}")
-        print(logit_stack[0, 0, :])
-        print(f"Sum of logit_stack: {logit_stack[0, 0, :].sum()}")
-        print(f"logits_offsets shape: {logits_offsets.shape}")
-        print(logits_offsets[0, 0, :])
-        print(f"Sum of logits_offsets: {logits_offsets[0, 0, :].sum()}")
+        # print(f"logits shape: {logits.shape}")
+        # print(logits[0, 0, :])
+        # print(f"Sum of logits: {logits[0, 0, :].sum()}")
+        # print(f"logit_stack shape: {logit_stack.shape}")
+        # print(logit_stack[0, 0, :])
+        # print(f"Sum of logit_stack: {logit_stack[0, 0, :].sum()}")
+        # print(f"logits_offsets shape: {logits_offsets.shape}")
+        # print(logits_offsets[0, 0, :])
+        # print(f"Sum of logits_offsets: {logits_offsets[0, 0, :].sum()}")
 
-        import sys
+        # import sys
 
-        sys.exit()
+        # sys.exit()
 
         # logits have shape [batch_size, N, num_classes], non_padding_mask has shape [batch_size, N]
         # multiply the logits by the non_padding_mask to zero out the padding tokens
