cf28e42451cd3f84f5807117f6d9466a2ab8e7b1
Neo Yin <yinzhang@berkeley.edu>
2024-12-04 00:58:21 -0800

    new model debyug

--- DiffTransformerV4/DiffTransformerV4.py
+++ DiffTransformerV4/DiffTransformerV4.py
@@ -119,11 +119,6 @@ class MultiHeadAttentionClassifier(nn.Module):
         # apply a softmax to the logits at dim=2
         logits = F.softmax(logits, dim=-1)
 
-        print(logits.shape)
-
-        import sys
-        sys.exit()
-
         # logits have shape [batch_size, N, num_classes], non_padding_mask has shape [batch_size, N]
         # multiply the logits by the non_padding_mask to zero out the padding tokens
         logits = logits * non_padding_mask.unsqueeze(2)
@@ -133,9 +128,6 @@ class MultiHeadAttentionClassifier(nn.Module):
 
         # then divide by the sum of the non_padding_mask to get the average
         logits = logits / non_padding_mask.sum(dim=1).unsqueeze(1)
-
-        # apply a softmax to the logits
-        logits = F.softmax(logits, dim=2)
 
         print(logits.shape)
 
