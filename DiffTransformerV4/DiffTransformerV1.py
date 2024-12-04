66c814ab11eef639f63b8547f0464bcb28e70c7f
Neo Yin <yinzhang@berkeley.edu>
2024-12-04 14:44:47 -0800

    sanity check

--- DiffTransformerV4/DiffTransformerV1.py
+++ DiffTransformerV4/DiffTransformerV1.py
@@ -265,7 +265,5 @@ if __name__ == "__main__":
             num_heads=1,
             num_classes=9,
             use_flash_attention=True,
-            log_dir="2024-12-03_11:48",
-            experiment_name=f"{lr}",
             message=message,
         )
