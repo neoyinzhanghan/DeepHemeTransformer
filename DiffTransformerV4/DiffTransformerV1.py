df738816bf6d0afeab29275bfe8555b6cbc33bb2
Neo Yin <yinzhang@berkeley.edu>
2024-12-16 02:25:54 -0800

    full no stochasticity training

--- DiffTransformerV4/DiffTransformerV1.py
+++ DiffTransformerV4/DiffTransformerV1.py
@@ -381,7 +381,7 @@ if __name__ == "__main__":
     feature_stacks_dir = "/media/ssd1/neo/DiffTransformerV1DataMini/feature_stacks"
     logit_stacks_dir = "/media/ssd1/neo/DiffTransformerV1DataMini/logit_stacks"
     diff_data_path = (
-        "/media/ssd1/neo/DiffTransformerV1DataMini/subsampled_split_diff_data.csv"
+        "/media/ssd1/neo/DiffTransformerV1DataMini/split_diff_data.csv"
     )
 
     message = "Testing different learning rates for the simple transformer model using the full mini dataset with the random subsample data augmentation, using simple L2 loss and the AR_acc metric."
