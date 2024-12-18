ca2a144cbb2de5a2520c255e48c90f9a8cdc9bcc
Neo Yin <yinzhang@berkeley.edu>
2024-12-18 15:26:42 -0800

    myeloma training script

--- MyelomaHacking/train.py
+++ MyelomaHacking/train.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
     diff_data_path = "/media/hdd3/neo/DiffTransformerV1DataMini/split_diff_data.csv"  # Replace with your CSV file
     dx_data_path = "/media/hdd3/neo/dx_data_test.csv"  # Replace with your CSV file
     batch_size = 16
-    num_epochs = 200
+    num_epochs = 1000
     num_gpus = [
         1
     ]  # The index of the gpu if in a list, otherwise the number to distribute among
