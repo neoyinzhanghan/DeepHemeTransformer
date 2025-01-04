3c4d5aadd9b3e24d4fe8c96f25d72b20465aea65
Neo Yin <yinzhang@berkeley.edu>
2025-01-03 21:13:50 -0800

    service update

--- MyelomaHacking/myeloma_prediction/get_high_plasma_cell_slide_cells.py
+++ MyelomaHacking/myeloma_prediction/get_high_plasma_cell_slide_cells.py
@@ -1,11 +1,11 @@
 import os
+from tqdm import tqdm
 
 result_dir_path = "/media/hdd4/neo/results_dir"
 
 
 # find the subdirs in the result_dir_path that does not start with ERROR_
 
-subdirs = [f for f in os.listdir(result_dir_path) if os.path.isdir(os.path.join(result_dir_path, f)) and not f.startswith("ERROR_")]
-
+subdirs = os.listdir(result_dir_path)
 # print the number of subdirs found
 print(f"Number of subdirs found: {len(subdirs)}")
