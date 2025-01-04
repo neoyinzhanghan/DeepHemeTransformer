9319fd5bb7c1888232395c5cd1fd4f6b2f9b278d
Neo Yin <yinzhang@berkeley.edu>
2025-01-04 10:47:08 -0800

    get elevated plasma cell slides

--- MyelomaHacking/myeloma_prediction/get_myeloma_plasma_distribution.py
+++ MyelomaHacking/myeloma_prediction/get_myeloma_plasma_distribution.py
@@ -2,6 +2,7 @@ import pandas as pd
 from LLRunner.read.SST import sst
 from get_copath_data import get_path_data, get_diff
 
+sst.load_dataframe()
 df = sst.df
 
 # get the accession_number column of df as a list of strings
