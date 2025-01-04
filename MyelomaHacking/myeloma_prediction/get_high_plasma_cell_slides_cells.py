1e57caa08aa036e97f0caefc7ac0fea91971547f
Neo Yin <yinzhang@berkeley.edu>
2025-01-04 10:20:56 -0800

    get elevated plasma cell slides

---
+++ MyelomaHacking/myeloma_prediction/get_high_plasma_cell_slides_cells.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+
+result_dir_path = "/media/hdd4/neo/results_dir"
+pipeline_run_history_path = "/media/hdd4/neo/results_dir/pipeline_run_history.csv"
+diff_data_path = "/media/hdd3/neo/diff_data_processed.csv"
+
+# open the diff_data_processed.csv file
+diff_data = pd.read_csv(diff_data_path)
+
+# replace na values with 0
+diff_data.fillna(0, inplace=True)
+
+# find the rows where the column plasma cells is >= 75
+high_plasma_cell_slides = diff_data[diff_data["plasma cells"] >= 75]
+
+# print the number of rows where the column plasma cells is >= 75
+print(f"Total number of rows: {len(diff_data)}")
+print(f"Number of rows where plasma cells >= 75: {len(high_plasma_cell_slides)}")
