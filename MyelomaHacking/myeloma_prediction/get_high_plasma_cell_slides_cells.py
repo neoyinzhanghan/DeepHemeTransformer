a4e6e573959b3acb56e9b84b2e1b041178f7208e
Neo Yin <yinzhang@berkeley.edu>
2025-01-04 10:29:12 -0800

    get elevated plasma cell slides

--- MyelomaHacking/myeloma_prediction/get_high_plasma_cell_slides_cells.py
+++ MyelomaHacking/myeloma_prediction/get_high_plasma_cell_slides_cells.py
@@ -18,3 +18,6 @@ high_plasma_cell_slides = diff_data[diff_data["plasma cells"] >= threshold]
 # print the number of rows where the column plasma cells is >= 75
 print(f"Total number of rows: {len(diff_data)}")
 print(f"Number of rows where plasma cells >= {threshold}: {len(high_plasma_cell_slides)}")
+
+# save the high_plasma_cell_slides to a csv file at /media/hdd3/neo/high_plasma_cell_slides.csv
+high_plasma_cell_slides.to_csv("/media/hdd3/neo/high_plasma_cell_slides.csv", index=False)
