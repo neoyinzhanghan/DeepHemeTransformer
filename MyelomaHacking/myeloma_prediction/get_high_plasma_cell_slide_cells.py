4ccb20bdb560fff3449726fccd6d442fea3354c0
Neo Yin <yinzhang@berkeley.edu>
2025-01-03 21:16:34 -0800

    service update

--- MyelomaHacking/myeloma_prediction/get_high_plasma_cell_slide_cells.py
+++ MyelomaHacking/myeloma_prediction/get_high_plasma_cell_slide_cells.py
@@ -1,19 +1,21 @@
 import os
+import pandas as pd
 from tqdm import tqdm
 
 result_dir_path = "/media/hdd4/neo/results_dir"
+pipeline_run_history_path = "/media/hdd4/neo/results_dir/pipeline_run_history.csv"
 
 
 # find the subdirs in the result_dir_path that does not start with ERROR_
 
 subdirs = os.listdir(result_dir_path)
-good_subdirs = []
-
-
-for subdir in tqdm(subdirs):
-    if not subdir.startswith("ERROR_"):
-        good_subdirs.append(subdir)
 
 # print the number of subdirs found
 print(f"Number of subdirs found: {len(subdirs)}")
-print(f"Number of good subdirs found: {len(good_subdirs)}")
+
+# open the pipeline_run_history.csv file
+pipeline_run_history = pd.read_csv(pipeline_run_history_path)
+
+# print how many rows in total and how many rows have error equal to False
+print(f"Total rows: {len(pipeline_run_history)}")
+print(f"Rows with error=False: {len(pipeline_run_history[pipeline_run_history['error'] == False])}")
